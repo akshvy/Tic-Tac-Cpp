0d7ee760354f069665e4b39ec6a4d8bf010ffbd5
akshvy <akshayroy217@gmail.com>
2026-04-17 12:05:07 +0530

    Add initial implementation of Tic-Tac-Toe game with board display

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void drawBoard(const vector<char>& board) {
+    // Clear the terminal screen (works on Linux/Debian)
+    system("clear"); 
+
+    cout << "\n Tic-Tac-Toe\n";
+    cout<<"___________________\n";
+    cout<<"|     |     |     |\n";
+    cout<<"|  "<<board[0]<<"  |  "<<board[1]<<"  |  "<<board[2]<<"  |\n";
+    cout<<"|_____|_____|_____|\n";
+    cout<<"|     |     |     |\n";
+    cout<<"|  "<<board[3]<<"  |  "<<board[4]<<"  |  "<<board[5]<<"  |\n";
+    cout<<"|_____|_____|_____|\n";
+    cout<<"|     |     |     |\n";
+    cout<<"|  "<<board[6]<<"  |  "<<board[7]<<"  |  "<<board[8]<<"  |\n";
+    cout<<"|_____|_____|_____|\n";
+}
+
+int main() {
+    vector<char> board = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int choice;
+    char player = 'x';
+    bool gamerunning = true;
+    
+    drawBoard(board);
+    
+    cout << "Player " <<  player << ", enter your move (1-9): ";
+    cin >> choice;
+
+    
+    return 0;
+}
