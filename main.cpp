4a88649b432488957e4039619f12be29bf868754
akshvy <akshayroy217@gmail.com>
2026-04-17 12:27:45 +0530

    Add win and draw checking functions to Tic-Tac-Toe game

--- main.cpp
+++ main.cpp
@@ -43,8 +43,29 @@ int main() {
         cin.get();
     }
 
-    
-    
+    bool checkWin(const vector<char>& board){
+
+        if (board[0] == board[1] == board[2]) return true;
+        if (board[3] == board[4] == board[5]) return true;
+        if (board[6] == board[7] == board[8]) return true;
+
+        if (board[0] == board[3] == board[6]) return true;
+        if (board[1] == board[4] == board[7]) return true;
+        if (board[2] == board[5] == board[8]) return true;
+
+        if (board[0] == board[4] == board[8]) return true;
+        if (board[2] == board[4] == board[6]) return true;
+        return false;
+    }
+
+    bool checkDraw(const std::vector<char>& board) {
+    for (int i = 0; i < 9; i++) {
+        if (board[i] != 'X' && board[i] != 'O') {
+            return false;
+        }
+    }
+    return true;
+}
 
 
     return 0;
