30c0c58e661142dcc747c847167c6ed26f4a5c3f
akshvy <akshayroy217@gmail.com>
2026-04-17 18:27:04 +0530

    Implement win and draw checking logic in Tic-Tac-Toe game

--- main.cpp
+++ main.cpp
@@ -16,7 +16,33 @@ void drawBoard(const vector<char>& board) {
     cout<<"|     |     |     |\n";
     cout<<"|  "<<board[6]<<"  |  "<<board[7]<<"  |  "<<board[8]<<"  |\n";
     cout<<"|_____|_____|_____|\n";
-}
+
+    }
+
+
+    bool checkWin(const vector<char>& board) {
+
+        if (board[0] == board[1] == board[2]) return true;
+        if (board[3] == board[4] == board[5]) return true;
+        if (board[6] == board[7] == board[8]) return true;
+
+        if (board[0] == board[3] == board[6]) return true;
+        if (board[1] == board[4] == board[7]) return true;
+        if (board[2] == board[5] == board[8]) return true;
+
+        if (board[0] == board[4] == board[8]) return true;
+        if (board[2] == board[4] == board[6]) return true;
+        return false;
+    }
+
+    bool checkDraw(const vector<char>& board) {
+    for (int i = 0; i < 9; i++) {
+        if (board[i] != 'X' && board[i] != 'O') {
+            return false;
+        }
+    }
+    return true;
+    }
 
 int main() {
     vector<char> board = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -43,29 +69,26 @@ int main() {
         cin.get();
     }
 
-    bool checkWin(const vector<char>& board){
 
-        if (board[0] == board[1] == board[2]) return true;
-        if (board[3] == board[4] == board[5]) return true;
-        if (board[6] == board[7] == board[8]) return true;
+        if (choice >= 1 && choice <= 9 && board[choice - 1] != 'X' && board[choice - 1] != 'O') {
+            board[choice - 1] = player;
 
-        if (board[0] == board[3] == board[6]) return true;
-        if (board[1] == board[4] == board[7]) return true;
-        if (board[2] == board[5] == board[8]) return true;
+            if (checkWin(board)) {
+                drawBoard(board);
+                cout << "Player " << player << " WINS! \n";
+                gamerunning = false; 
+            } 
 
-        if (board[0] == board[4] == board[8]) return true;
-        if (board[2] == board[4] == board[6]) return true;
-        return false;
-    }
+            else if (checkDraw(board)) {
+                drawBoard(board);
+                cout << "It's a DRAW! \n";
+                gamerunning = false;
+            } 
 
-    bool checkDraw(const std::vector<char>& board) {
-    for (int i = 0; i < 9; i++) {
-        if (board[i] != 'X' && board[i] != 'O') {
-            return false;
+            else {
+                player = (player == 'X') ? 'O' : 'X';
+            }
         }
-    }
-    return true;
-}
 
 
     return 0;
